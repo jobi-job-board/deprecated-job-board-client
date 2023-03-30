a729868f5f38b807801dc165c3dc5bf3d588427e
Renata Reinartz <rcreinartz@gmail.com>
2023-03-29 22:43:45 -0600

    naming-convention

--- src/components/About.js
+++ src/components/About.js
@@ -7,15 +7,15 @@ const About = () => {
           <h2>Get the job of your dreams quickly.</h2>
         </div>
         <div className="about-text flex-col">
-          <h2>Get the job of your dreams quickly.</h2>
+          <h2 className="secondary-header">Get the job of your dreams quickly.</h2>
           <p>
             Inciddnt ut labore et dolor magna aliu. enim ad mim venam, quis
             nostru
           </p>
           <ul>
-            <li>✔ Seamless searching</li>
-            <li>✔ Protected payments, every time</li>
-            <li>✔ Wide range of job categories</li>
+            <li><h6>✔ Seamless searching</h6></li>
+            <li><h6>✔ Protected payments, every time</h6></li>
+            <li><h6>✔ Wide range of job categories</h6></li>
           </ul>
           <button className="btn-square">Learn More</button>
         </div>
