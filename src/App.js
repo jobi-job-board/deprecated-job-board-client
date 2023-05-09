da34824ea304a5a7f5afc9ef4c3a4ce86d8d5de7
Renata Reinartz <rcreinartz@gmail.com>
2023-05-08 23:24:00 -0600

    add-slick-css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import logo from "./assets/logo.png";
-import firebase from "./firebase";
+import logo from './assets/logo.png';
+import firebase from './firebase';
 // Assets
-import "./App.scss";
+import './App.scss';
 
-import { Routes, Route } from "react-router-dom";
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
+import { Routes, Route } from 'react-router-dom';
 // Pages
-import Home from "./components/Home";
-import Register from "./components/Register";
+import Home from './components/Home';
+import Register from './components/Register';
 
 function App() {
   return (
